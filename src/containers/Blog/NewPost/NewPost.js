aa609c516f3afc770e29e313fe504645f596dfa9
samSterns <sam.g.sterns@gmail.com>
2020-12-22 09:23:36 -0800

    added routing

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -43,7 +43,7 @@ class NewPost extends Component {
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
                     <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
+                    <option value="Man">Manu</option>
                 </select>
                 <button onClick={this.postDataHandler}>Add Post</button>
             </div>
